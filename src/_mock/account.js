f3b0701f7e216a462426b1cb6302d50357f5b558
mohammedsaif-fb <mohammedsaifrockstar@gmail.com>
2022-12-07 17:56:46 +0000

    vercel deploy

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'MIH Max',
+  email: 'max@mih.com',
+  photoURL: '/assets/images/avatars/avatar_default.jpg',
+};
+
+export default account;
